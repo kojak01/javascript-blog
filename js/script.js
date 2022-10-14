b3c021d1de42ac486926d0e3604343e19e61d406
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-14 23:01:32 +0200

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -28,21 +28,25 @@ const titleClickHandler = function(){
 */
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-        for(let activeLink of activeLinks){
+
+    for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
-        }
+    }
   
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArtickles = document.querySelectorAll('.posts article.active');
-        for(let activeArticle of activeArtickles){
+
+    for(let activeArticle of activeArtickles){
         activeArticle.classList.remove('active');
-        }    
+    }    
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */
