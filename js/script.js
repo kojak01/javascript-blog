eefc69f12a651457443df2176e1e7aad4db234ff
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-16 17:46:58 +0200

    add create HTML of the link

--- js/script.js
+++ js/script.js
@@ -82,13 +82,14 @@ function generateTitleLinks(){
     /* [DONE] get the article id */
     const articleId = article.getAttribute('id');
     console.log(articleId);
-    /* find the title element */
+    /* [DONE]find the title element */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
     console.log(articleTitle);
     /* get the title from the title element */
 
-    /* create HTML of the link */
-
+    /* [DONE]create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
     /* insert link into titleList */
   }
 }
