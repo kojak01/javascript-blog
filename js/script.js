ad8fd792a7dc2853d7e3ade9ed21a92ea9847bf5
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-23 22:42:54 +0200

    add Array and loop for each tag

--- js/script.js
+++ js/script.js
@@ -103,13 +103,15 @@ function generateTags(){
     const articleTags = article.getAttribute('data-tags');
     console.log(articleTags);
     /* split tags into array */
-
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
     /* START LOOP: for each tag */
-
-    /* generate HTML of the link */
+    for(let tag of articleTagsArray){
+      console.log(tag);
+      /* generate HTML of the link */
 
     /* add generated code to html variable */
-
+    }
     /* END LOOP: for each tag */
 
     /* insert HTML of all the links into the tags wrapper */
