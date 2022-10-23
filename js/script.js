7d169e07c37df0c175d6df88b5032738f910415d
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-24 00:12:30 +0200

    add const tag

--- js/script.js
+++ js/script.js
@@ -133,7 +133,8 @@ function tagClickHandler(event){
   const href = clickedElement.getAttribute('href');
   console.log(href);
   /* make a new constant "tag" and extract tag from the "href" constant */
-
+  const tag = href.replace('#tag-', '');
+  console.log(tag);
   /* find all tag links with class active */
 
   /* START LOOP: for each active tag link */
