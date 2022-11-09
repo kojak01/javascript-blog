f0b54a144cb56a1a2d3a5095db1e537051a3b0bb
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-09 20:45:04 +0100

    reviseing the basics JS

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// let sum = (a, b) => {
+//     console.log(a * b);
+// };
+
+// sum(32, 23);
+
+
+// let subtraction = (a , b) => {
+//     const sub = a - b;
+//     return sub;
+// };
+
+// alert(subtraction(3232, 25));
+
+
+//confirm('are you sure?');
+
+
+// prompt('how old are you?');
+
+// const contentBox = document.getElementById('content-box');
+
+// const mainHeader = document.querySelector('.main-header');
