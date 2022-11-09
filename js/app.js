5a9935aa716702e00d308c535e93ec9fe13992e4
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-09 23:06:07 +0100

    example Arrays

--- js/app.js
+++ js/app.js
@@ -90,3 +90,91 @@
 
 // console.log(names);
 
+// const myArr = ['one', 'two', 3];
+// myArr.push(3);
+// console.log(myArr);
+
+// const person = {
+//     name: 'Kazik',
+//     surname: 'Kowalsky',
+// };
+
+// console.log(person.name);
+// let b = 2,
+// c = 3;
+// let a = b ** c
+
+// console.log(a);
+
+// const randomNumber = Math.floor(Math.random() * 52) + 1;
+// console.log(randomNumber);
+
+// const Table1 = ['Poland', 'Ukraine'];
+// console.log(Table1);
+// const firstIndexTable1 = 0;
+
+// const firstIndex = Table1[firstIndexTable1];
+// console.log(firstIndex);
+
+
+// const indexOfPoland = Table1.indexOf('France');
+// console.log('indexOfPoland: ', indexOfPoland);
+
+
+
+
+// const allRemovedValues = myArray.splice(startAtIndex, numberOfElements);
+
+//         categories.push('fruits');
+//         categories.push('food');
+//         categories.push('phones');
+//         console.log('categories:', categories); // categories: ['animals', 'travel', 'fruits', 'food', 'phones']
+
+//         const indexOfFruits = categories.indexOf('fruits');
+//         console.log('indexOfFruits:', indexOfFruits); // indexOfFruits: 2
+
+//         const removedValues = categories.splice(indexOfFruits, 1);
+//         console.log('removedValues:', removedValues); // removedValues: ['fruits']
+//         console.log('categories:', categories); // categories: ['animals', 'travel', 'food', 'phones']
+
+//         const indexOfFood = categories.indexOf('food');
+//         console.log('indexOfFood:', indexOfFood); // indexOfFood: 2
+
+//         categories.splice(indexOfFood, 2);
+//         console.log('categories:', categories); // categories: ['animals', 'travel']
+
+// const MyAnimalArray = ['dog', 'cat'];
+// MyAnimalArray.push('snake', 'elephant', 'monkey'); 
+
+// const indexofDog = MyAnimalArray.indexOf('dog');
+// console.log(indexofDog); // 0
+
+// const removeDog = MyAnimalArray.splice(indexofDog, 5);
+// console.log(removeDog);
+// console.log(MyAnimalArray);
+
+
+// const keyWordsArray = ['animals', 'cars', 'plants', 'worms', 'stones'];
+// const firstCategory = keyWordsArray[3];
+// console.log(firstCategory);
+// const indexofCars = keyWordsArray.indexOf('cars');
+// console.log(indexofCars);
+
+// const removeCars = keyWordsArray.splice(indexofCars, 1);
+// console.log(keyWordsArray);
+
+// const whiteSpace = keyWordsArray.join(', ');
+// console.log(whiteSpace);
+
+// const HtmlWithArray = '<ul><li>' + keyWordsArray.join('</li><li>') + '</li><li>';
+// console.log('HtmlWithArray: ', HtmlWithArray);
+
+// const aniMals = ['cat', 'dog', 'snake', 'monkey', 'bat', 'scorpion'];
+// const animalsArray = aniMals.split();
+// const lengthAnimals = aniMals.length;
+// console.log(lengthAnimals);
+
+// console.log(indexOfSnake = aniMals.indexOf('snake'));
+
+// let newArrayAnimals = aniMals.slice();
+// console.log(newArrayAnimals);
