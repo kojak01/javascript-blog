183473aba113bbd223ee2f191df50a8ca0646e77
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-09 21:14:14 +0100

    more examples

--- js/app.js
+++ js/app.js
@@ -21,3 +21,32 @@
 // const contentBox = document.getElementById('content-box');
 
 // const mainHeader = document.querySelector('.main-header');
+
+// const links = document.querySelectorAll('.titles a');
+
+// CLASSLIST
+
+// const div = document.querySelector('#testing-div');
+// console.log(div);
+// div.classList.add('active');
+// console.log(div.classList.contains('active'));
+// div.classList.remove('active');
+// console.log(div.classList.contains('active'));
+// div.classList.toggle('active');
+// console.log(div.classList.contains('active'));
+
+//INNER HTML
+
+// let div = document.querySelector('#testing-div');
+// console.log(div.innerHTML);
+
+// div.innerHTML = '<strong>Hello!</strong>';
+
+// INSERT ADJACENTHTML
+
+// const div = document.querySelector('#testing-div');
+// const newCode = '<strong> Hello world! </strong>';
+// div.insertAdjacentHTML('beforeend', newCode);
+// div.insertAdjacentHTML('beforebegin', newCode);
+// div.insertAdjacentHTML('afterbegin', newCode);
+// div.insertAdjacentHTML('afterend', newCode);
