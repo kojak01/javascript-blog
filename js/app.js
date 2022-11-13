9c4bc77974a4bfeb274d34cbe30adc6c86d1ed6f
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-13 21:14:06 +0100

    example of a list in handlebars

--- js/app.js
+++ js/app.js
@@ -1,15 +1,39 @@
-const tplHelloSource = document.querySelector('#template-hello').innerHTML;
-const tplHello = Handlebars.compile(tplHelloSource);
-const dataHello = {firstName: 'John', lastName: 'Smith'};
-let generatedHTML = tplHello(dataHello);
+// const tplHelloSource = document.querySelector('#template-hello').innerHTML;
+// const tplHello = Handlebars.compile(tplHelloSource);
+// const dataHello = {firstName: 'John', lastName: 'Smith'};
+// let generatedHTML = tplHello(dataHello);
+// const targetElement = document.body;
+// targetElement.insertAdjacentHTML('beforeend', generatedHTML);
+// console.log('tplHello:');
+// console.log(tplHelloSource);
+// console.log('=========');
+// console.log('dataHello:');
+// console.log(dataHello);
+// console.log('=========');
+// console.log('generatedHTML:');
+// console.log(generatedHTML);
+// console.log('=========');
+
+
+const tplProductListSource = document.querySelector('#template-product-list').innerHTML;
+const tplProductList = Handlebars.compile(tplProductListSource);
+const productListData = {
+ title: 'Great offers!',
+ products: {
+ 'product-football': {
+ name: 'Football',
+ price: '$10'
+ },
+ 'product-volleyball': {
+ name: 'Volleyball',
+ price: '$8'
+ },
+ 'product-basketball': {
+ name: 'Basketball',
+ price: '$12'
+ }
+ }
+};
+let generatedHTML = tplProductList(productListData);
 const targetElement = document.body;
 targetElement.insertAdjacentHTML('beforeend', generatedHTML);
-console.log('tplHello:');
-console.log(tplHelloSource);
-console.log('=========');
-console.log('dataHello:');
-console.log(dataHello);
-console.log('=========');
-console.log('generatedHTML:');
-console.log(generatedHTML);
-console.log('=========');
