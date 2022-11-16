3f87411b1ced5277863f20f988e2a15446228038
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-16 21:28:30 +0100

    reference examples

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// let personOne = 'John';
+// let personTwo = personOne;
+// personTwo = personOne + ' II';
+// console.log('Person one', personOne);
+// console.log('Person two', personTwo);
+
+
+// const personOne = { firstName: 'John', lastName: 'Doe' };
+// const personTwo = personOne;
+// personTwo.firstName = 'Amanda';
+// console.log('Person one', personOne);
+// console.log('Person two', personTwo);
+
+// let personAgeOne = 10;
+// let personAgeTwo = personAgeOne;
+// personAgeTwo += 2;
+// console.log('Person one', personAgeOne);
+// console.log('Person two', personAgeTwo); 
+
+// const namesOne = ['John', 'Amanda'];
+// const namesTwo = namesOne;
+// namesTwo.push('Thomas');
+// console.log(namesOne);
+// console.log(namesTwo);
+
+// const label = 'Names of people';
+// const names = ['John', 'Amanda'];
+// function prepareAndShowNames(namesArr, title) {
+//  title = '==' + title + '==';
+//  namesArr.push('Thomas');
+//  console.log(title, namesArr);
+//  console.log(label, names);
+// }
+// prepareAndShowNames(names, label);
+
+// const namesOne = ['John', 'Amanda'];
+// let namesTwo = namesOne;
+// namesTwo.push('Thomas');
+// namesTwo = []; //<== declaration new table
+// console.log(namesOne);
+// console.log(namesTwo);
+
+// const person = { firstName: '', lastName: '' };
+// const name = person.firstName; //<== empty string!
+// const personOne = person;
+// personOne.firstName = 'John';
+// personOne.lastName = 'Doe';
+// const personTwo = person;
+// personTwo.firstName = 'Amanda';
+// personTwo.lastName = 'Doe';
+// console.log(name, personOne, personTwo);
