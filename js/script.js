28910e002783d9e6c6f220b1100e34df65420e1e
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-12-29 22:29:51 +0100

    exercise Number 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-// Exercises Number 1.
-// Create female name boards.
-const names = ['Kasia', 'Tomek', 'Amanda', 'Maja'];
+// Exercises Number 2.
+// create two arrays
+// 1. names array
+// 2. salary array
 
-const newArray = [];
-
-for(const name of names){
-  if(name.slice(-1) === 'a'){
-    newArray.push(name);
-  }
+const employees = {
+  john: {
+    name: 'John Doe',
+    salary: 3000
+  },
+  amanda: {
+    name: 'Amanda Doe',
+    salary: 4000
+  },
 }
 
-console.log(newArray);
+const employeesNames = [];
+const employeesSalaries = [];
+
+for(const employeeID in employees){
+  const employee = employees[employeeID];
+  console.log(employee);
+
+  const empName = employee.name.split(' ')[0];
+  console.log(empName);
+  employeesNames.push(empName);
+  employeesSalaries.push(employee.salary);
+}
+
+console.log(employeesNames);
+console.log(employeesSalaries);
