f9ec8400b77762f16ab88578135dc9103cd94ab5
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2023-01-01 22:25:49 +0100

    exercise number 9

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-// Exercises Number 8 
-// Write a forEach function that takes two arguments:
-// any board, any callback function.
-// The task of the function should be to go through each 
-// element of the array and call the callback function for each separately.
-// The one we get in the second parameter. 
-// Importantly, this callback function should be called with one argument,
-//  equal to the currently handled element.
+// Exercises Number 9 
+// Write a formatName function that takes the first name
+// and last name as an argument, and then returns the corrected form.
+// Corrected, i.e. one in which only the first letter of the name will be capitalized,
+// and the rest will be lowercase.
 
-forEach(['John', 'Amanda', 'Thomas'], function(item) { console.log(item); });
+function formatName(name){
+  const firstNameAndLastName = name.split(' ');
+  let firstName = firstNameAndLastName[0];
+  let lastName = firstNameAndLastName[1];
 
-function forEach(arr, cb){
-  for(const element of arr) {
-    cb(element);
-  }
+  firstName = firstName.charAt(0).toUpperCase() + firstName.substr(1).toLowerCase()
+  lastName = lastName.charAt(0).toUpperCase() + lastName.substr(1).toLowerCase()
+
+  return firstName + ' ' + lastName;
 }
+
+console.log(formatName('aMAnDa dOE')); // returns Amanda Doe
+console.log(formatName('AMANDA DOE')); // returns Amanda Doe
+console.log(formatName('john DOE')); // returns John Doe
