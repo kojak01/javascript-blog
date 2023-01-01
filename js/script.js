51d8dbd28f623ef121ccf146d9cb61288cc76809
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2023-01-01 21:32:54 +0100

    exercise Number 6

--- js/script.js
+++ js/script.js
@@ -1,28 +1,18 @@
-// Exercises Number 6.
-// Write a filterEmployees function that takes two arguments:
-// an array with objects of the structure { name: 'First name', salary: salary-amount }
-// the minimum value of the range,
-// the maximum value of the range.
+// Exercises Number 7 
+// Write a function that takes one argument (any object)
+// and then displays information
+// about all its properties in the console.
 
-function filterEmployees(arr, min, max) {
-  const filteredArray = [];
-
-  for(const empl of employees) {
-    if(empl.salary > min && empl.salary < max) {
-      filteredArray.push(empl);
-    }
-  }
-
-  return filteredArray;
+const object = {
+  firstName: 'John',
+  lastName: 'Doe'
 }
 
-const employees = [
-  { name: 'Amanda Doe', salary: 3000 },
-  { name: 'John Doe', salary: 4000 },
-  { name: 'Claire Downson', salary: 2000 },
-  { name: 'Freddie Clarkson', salary: 6000 },
-  { name: 'Thomas Delaney', salary: 8200 }
-];
+function showAnyObject(obj){
+  for(objID in object){
+    const param = object[objID];
+    console.log(objID + ': ' + param);
+  }
+}
 
-const filteredEmployees = filterEmployees(employees, 2000, 8000);
-console.log(filteredEmployees);
+showAnyObject(object);
